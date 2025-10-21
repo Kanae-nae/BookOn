3c0d3ecb4a387a36a3c654c5fa1e9a5c2c219a25
Saki <philitan.1245@gmail.com>
2025-10-21 09:16:05 +0900

    管理者側変更+ページ追加
    ・管理者側のヘッダー、サイドバー作成
    ・フッターとメニューを分離

--- admin/g16_login.php
+++ admin/g16_login.php
@@ -1,4 +1,4 @@
 <!-- 管理者側ホーム(G16) -->
-<?php require "../common/header.html"; ?>
-
+<?php require "../common/admin_header.html"; ?>
+<?php require '../common/admin_sidebar.html'; ?>
 <?php require '../common/footer.php'; ?>
