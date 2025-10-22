b76bdfb3395a8681b70b6dd44cf0a6930db2ecb2
Saki <2301088@s.asojuku.ac.jp>
2025-10-22 11:07:30 +0900

    ファイル追加
    変更用のファイル追加

---
+++ admin/g16_login.php
@@ -0,0 +1,4 @@
+<!-- 管理者側ホーム(G16) -->
+<?php require "../common/admin_header.html"; ?>
+
+<?php require '../common/footer.php'; ?>
---
+++ admin/g17_shohin.php
@@ -0,0 +1,5 @@
+<!-- 商品管理(G17) -->
+<?php require "../common/admin_header.html"; ?>
+<?php require '../common/admin_sidebar.html'; ?>
+
+<?php require '../common/footer.php'; ?>
