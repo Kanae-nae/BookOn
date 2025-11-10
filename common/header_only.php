83c5325119dfa0c68b68971b5b6204a2f6c03d30
yuduru0116 <yuduru.1025@icloud.com>
2025-11-10 09:44:50 +0900

    g4ログイン画面の追加

---
+++ common/header_only.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+
+<html lang="ja">
+
+<head>
+
+    <meta charset="UTF-8">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>ヘッダーのみ</title>
+
+    <link rel="stylesheet" href="css/header_only.css">
+
+</head>
+
+<body>
+
+    <header class="logo-only-header">
+
+        <div class="logo">
+
+            <a href="index.php">
+                <img src="image/logo.png" alt="BOOK ON Logo">
+            </a>
+
+        </div>
+
+    </header>
+</body>
+
+</html>
