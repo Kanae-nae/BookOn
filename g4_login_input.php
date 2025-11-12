3d6c0850e528e5149ea3d81ac180db075fd87fef
Kanae-nae <2401195@s.asojuku.ac.jp>
2025-11-12 10:12:09 +0900

    G4_ログイン：不必要な行を削除

--- g4_login_input.php
+++ g4_login_input.php
@@ -6,7 +6,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>会員登録・ログイン</title>
-    <link rel="stylesheet" href="css/header_only.css">
     <link rel="stylesheet" href="css/g4.css">
 </head>
 
